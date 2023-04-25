9d0e3b0cfd28b2151f773bd74ac4fe557850eee8
Zala Starc <zala.starc@gmail.com>
2023-04-25 16:25:33 +0200

    zakljucil dodatno analizo

--- analiza.py
+++ analiza.py
@@ -20,6 +20,8 @@ def relativne_frekvence(besede):
         for crka, frek in frekvence.items():
             print(f"{crka},{frek:.2e}", file=f)
 
+def dodatno():
+    return 21
 
 vse_besede = nalozi_besede()
 relativne_frekvence(vse_besede)
